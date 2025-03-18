24b9bc425885b00730ce0f7195d94c2ca57941ab
Letícia Keuffer <lekeuffer@gmail.com>
2025-03-18 09:33:27 -0300

    criando classe peca

---
+++ Xadrez/Domain/Application/Tabuleiro/Peca.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xadrez.Domain.Application.Tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor cor {  get; protected set; }
+        public int qntdMovimentos { get; protected set; }
+        //public Tabuleiro tabuleiro { get; protected set; }
+    }
+}
