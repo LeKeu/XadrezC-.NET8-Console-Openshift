aaddb0ef5fbfb915ce7f3e2b728c0d79436d2859
Letícia Keuffer <lekeuffer@gmail.com>
2025-03-18 09:28:36 -0300

    criando obj Posicao

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Xadrez.Domain.Application.Tabuleiro;
+
 Console.WriteLine("Hello, World!");
+
+Posicao P = new(3, 5);
+Console.WriteLine("posicao ; " + P);
