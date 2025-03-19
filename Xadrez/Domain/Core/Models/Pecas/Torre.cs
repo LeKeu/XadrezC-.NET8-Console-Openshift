6849f04bb8de70c205e76c89be752ffef92f270e
Letícia Keuffer <lekeuffer@gmail.com>
2025-03-19 13:38:05 -0300

    movimentações torre

--- Xadrez/Domain/Core/Models/Pecas/Torre.cs
+++ Xadrez/Domain/Core/Models/Pecas/Torre.cs
@@ -13,5 +13,58 @@ namespace Xadrez.Domain.Core.Models.Pecas
         public Torre(Tabuleiro tab, EnumCor cor) : base(tab, cor) { }
 
         public override string ToString() => "T";
+
+        private bool PodeMover(Posicao pos)
+        {
+            Peca p = tabuleiro.RetornarPecaNaPosicao(pos);
+            return p == null || p.cor != cor;
+        }
+        public override bool[,] MovimentosPossiveis()
+        {
+            bool[,] matriz = new bool[tabuleiro.Linhas, tabuleiro.Colunas];
+            Posicao pos = new Posicao(0, 0);
+
+            // cima
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna);
+            while(tabuleiro.PosicaoEValida(pos) && PodeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+                if(tabuleiro.RetornarPecaNaPosicao(pos) != null && tabuleiro.RetornarPecaNaPosicao(pos).cor != cor)
+                    break;
+                pos.Linha = pos.Linha - 1;
+            }
+
+            // baixo
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna);
+            while (tabuleiro.PosicaoEValida(pos) && PodeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+                if (tabuleiro.RetornarPecaNaPosicao(pos) != null && tabuleiro.RetornarPecaNaPosicao(pos).cor != cor)
+                    break;
+                pos.Linha = pos.Linha + 1;
+            }
+
+            // direita
+            pos.DefinirValores(Posicao.Linha, Posicao.Coluna + 1);
+            while (tabuleiro.PosicaoEValida(pos) && PodeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+                if (tabuleiro.RetornarPecaNaPosicao(pos) != null && tabuleiro.RetornarPecaNaPosicao(pos).cor != cor)
+                    break;
+                pos.Coluna = pos.Coluna + 1;
+            }
+
+            // esquerda
+            pos.DefinirValores(Posicao.Linha, Posicao.Coluna - 1);
+            while (tabuleiro.PosicaoEValida(pos) && PodeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+                if (tabuleiro.RetornarPecaNaPosicao(pos) != null && tabuleiro.RetornarPecaNaPosicao(pos).cor != cor)
+                    break;
+                pos.Coluna = pos.Coluna - 1;
+            }
+
+            return matriz;
+        }
     }
 }
