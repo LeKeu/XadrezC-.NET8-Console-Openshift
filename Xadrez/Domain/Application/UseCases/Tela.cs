e8170cbe86910c597925dd0b4d89c92ac7118c83
Letícia Keuffer <lekeuffer@gmail.com>
2025-03-22 08:51:23 -0300

    printando xeque

--- Xadrez/Domain/Application/UseCases/Tela.cs
+++ Xadrez/Domain/Application/UseCases/Tela.cs
@@ -14,6 +14,8 @@ namespace Xadrez.Domain.Application.UseCases
             Console.WriteLine();
             Console.WriteLine($"Turno: {partida.Turno}");
             Console.WriteLine($"Aguardando Jogada: {partida.JogadorAtual}");
+            if(partida.Xeque)
+                Console.WriteLine($"XEQUE!");
         }
 
         public static void ImprimirPecasCapturadas(PartidaDeXadrez partida)
